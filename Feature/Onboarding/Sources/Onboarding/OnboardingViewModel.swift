a36074b290bef1da12aecf5ad2010de78c013e8a
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-02 17:23:02 +0900

    [Add] 온보딩 뷰, 뷰 모델 초기 구성

---
+++ Feature/Onboarding/Sources/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  OnboardingViewModel.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 쩡화니 on 6/2/24.
+//
+
+import SwiftUI
+import Combine
+import Protocols
+
+public final class OnboardingViewModel: BaseViewModelType {
+  
+  public var cancellables: Set<AnyCancellable> = .init()
+  
+  init() {
+    transform()
+  }
+  
+  public struct Input {
+    
+  }
+  
+  public struct Output {
+    
+  }
+  
+  func transform() {
+    
+  }
+}
