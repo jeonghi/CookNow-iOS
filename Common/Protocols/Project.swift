76ec30df71bb8d650494aaee43f67c9fd1fe79fc
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-02 17:21:55 +0900

    [Add] 기본 뷰 모델 타입 프로토콜 정의

---
+++ Common/Protocols/Project.swift
@@ -0,0 +1,13 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 쩡화니 on 6/2/24.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeLibrary(
+  name: ModuleNameSpace.Common.Protocols.rawValue
+)
