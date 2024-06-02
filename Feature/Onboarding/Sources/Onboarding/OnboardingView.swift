8a330b4a7cfeca0c77b7096d6e435ef9775ed5ac
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-02 21:36:56 +0900

    [Add] 소셜 로그인 버튼 온보딩 화면에 추가

--- Feature/Onboarding/Sources/Onboarding/OnboardingView.swift
+++ Feature/Onboarding/Sources/Onboarding/OnboardingView.swift
@@ -18,11 +18,32 @@ struct OnboardingView {
   }
 }
 
+// MARK: Constants
+extension OnboardingView {
+  private enum Metric {
+    static var titleTopPadding: CGFloat { 18 }
+    static var socialSignUpLabelTopPadding: CGFloat { 44 }
+    static var socialSignUpButtonStackHorizontalSpacing: CGFloat { 10 }
+    static var socialSignUpButtonStackTopPadding: CGFloat { 20 }
+  }
+}
+
 // MARK: Layout
 extension OnboardingView: View {
+  
   var body: some View {
     ZStack {
-      logo
+      VStack(spacing: 0) {
+        logo
+        title
+          .padding(.top, Metric.titleTopPadding)
+        socialSignUpLabel
+          .padding(.top, Metric.socialSignUpLabelTopPadding)
+        HStack(spacing: Metric.socialSignUpButtonStackHorizontalSpacing) {
+          socialSignUpButtonStack
+        }
+        .padding(.top, Metric.socialSignUpButtonStackTopPadding)
+      }
     }
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .background(
@@ -52,6 +73,64 @@ extension OnboardingView {
       .renderingMode(.original)
       .aspectRatio(contentMode: .fit)
       .frame(width: 192)
+  }
+  
+  /// 타이틀
+  private var title: some View {
+    Text("쿡 나우")
+      .font(
+        DesignSystemFontFamily
+          .Pretendard
+          .bold
+          .swiftUIFont(size: 39)
+      )
+  }
+  
+  private var socialSignUpLabel: some View {
+    Text("소셜 계정으로 간편 가입하기")
+      .font(
+        DesignSystemFontFamily
+          .Pretendard
+          .medium
+          .swiftUIFont(size: 14)
+      )
+  }
+  
+  private var socialSignUpButtonStack: some View {
+    ForEach(SocialLoginType.allCases, id: \.self) {
+      makeSocialLoginButton(type: $0)
+    }
+  }
+  
+  @ViewBuilder
+  private func makeSocialLoginButton(
+    type: SocialLoginType
+  ) -> some View {
+    Button(action: {type.closure?()}) {
+      type.iconImage
+        .resizable()
+        .renderingMode(.original)
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 54, height: 54)
+    }
+  }
+  
+  private enum SocialLoginType: CaseIterable {
+    case apple
+    case google
+    
+    var iconImage: Image {
+      switch self {
+      case .apple:
+        return Image(.appleLogin)
+      case.google:
+        return Image(.googleLogin)
+      }
+    }
+    
+    var closure: (() -> Void)? {
+      return nil
+    }
   }
 }
 
