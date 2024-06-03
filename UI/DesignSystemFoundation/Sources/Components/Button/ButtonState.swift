3864fec59b6745d9f3bbfebb24a4e7cc40e14e0e
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-04 01:22:08 +0900

    [Add] 버튼 상태 정의
    normal, pressed, disabled 상태 정의

---
+++ UI/DesignSystemFoundation/Sources/Components/Button/ButtonState.swift
@@ -0,0 +1,14 @@
+//
+//  ButtonState.swift
+//  DesignSystemFoundation
+//
+//  Created by 쩡화니 on 6/4/24.
+//
+
+import Foundation
+
+public enum ButtonState {
+  case normal
+  case pressed
+  case disabled
+}
