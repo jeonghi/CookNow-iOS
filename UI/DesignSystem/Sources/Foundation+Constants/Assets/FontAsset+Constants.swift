911baff52700d0472b0bb2e451470ecb99162edb
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-04 01:18:58 +0900

    [Add] 폰트 에셋 상수 추가
    bold, regular, semibold 정의

--- UI/DesignSystem/Sources/Foundation+Constants/Assets/FontAsset+Constants.swift
+++ UI/DesignSystem/Sources/Foundation+Constants/Assets/FontAsset+Constants.swift
@@ -7,11 +7,52 @@
 
 import DesignSystemFoundation
 
-private extension FontAsset.FontConfig {
+enum FontFamily: String {
+  case Pretendard
 }
 
-private extension FontAsset.Leading {
+public extension FontAsset.FontConfig {
+  static var bold: FontAsset.FontConfig = .init(
+    fontFamily: FontFamily.Pretendard.rawValue,
+    weight: .bold,
+    bundle: .module,
+    fileType: .otf
+  )
+  static var regular: FontAsset.FontConfig = .init(
+    fontFamily: FontFamily.Pretendard.rawValue,
+    weight: .regular,
+    bundle: .module,
+    fileType: .otf
+  )
+  
+  static var semibold: FontAsset.FontConfig = .init(
+    fontFamily: FontFamily.Pretendard.rawValue,
+    weight: .semiBold,
+    bundle: .module,
+    fileType: .otf
+  )
+}
+
+public extension FontAsset.Leading {
+  
 }
 
 public extension FontAsset {
+  static var title1: FontAsset = .init(.bold, size: 22)
+  static var title2: FontAsset = .init(.bold, size: 14)
+  static var bodyBold: FontAsset = .init(.bold, size: 13)
+  static var body: FontAsset = .init(.regular, size: 13)
+  static var caption: FontAsset = .init(.regular, size: 12)
 }
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+#Preview {
+  ScrollView {
+    Text("테스트1")
+      .font(Font.asset(.title1))
+  }
+}
+
+#endif
