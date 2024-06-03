4a981cbfb5c32e0171d4861be0d263d2d8b1d532
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-04 01:11:19 +0900

    [Add] 색상 에셋 상수 추가
    투명, primary 상수 정의

--- UI/DesignSystem/Sources/Foundation+Constants/Assets/ColorAsset+Constants.swift
+++ UI/DesignSystem/Sources/Foundation+Constants/Assets/ColorAsset+Constants.swift
@@ -9,9 +9,17 @@ import DesignSystemFoundation
 
 
 public extension ColorAsset {
+  
+  // MARK: Background
   static let bg100 = ColorAsset(named: "bg100", bundle: .module)
   static let bg200 = ColorAsset(named: "bg200", bundle: .module)
   static let bgMain = ColorAsset(named: "bg300", bundle: .module)
+  static let clear = white.with(alpha: 0)
+  
+  // MARK: Primary
+  static let primary200 = ColorAsset(named: "primary200", bundle: .module)
+  static let primary500 = ColorAsset(named: "primary500", bundle: .module)
+  static let primary700 = ColorAsset(named: "primary700", bundle: .module)
 }
 
 #if canImport(SwiftUI)
