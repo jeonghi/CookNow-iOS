65ba318b17fdd6fd5319877dc37b8c2f23a9c0c5
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-14 01:17:49 +0900

    [Add] 색상 에셋 상수값 추가 정의

--- UI/DesignSystem/Sources/Foundation+Constants/Assets/ColorAsset+Constants.swift
+++ UI/DesignSystem/Sources/Foundation+Constants/Assets/ColorAsset+Constants.swift
@@ -14,7 +14,6 @@ public extension ColorAsset {
   static let bg100 = ColorAsset(named: "bg100", bundle: .module)
   static let bg200 = ColorAsset(named: "bg200", bundle: .module)
   static let bgMain = ColorAsset(named: "bg300", bundle: .module)
-  static let clear = white.with(alpha: 0)
   
   // MARK: Primary
   static let primary100 = ColorAsset(named: "primary100", bundle: .module)
@@ -25,6 +24,13 @@ public extension ColorAsset {
   static let primary600 = ColorAsset(named: "primary600", bundle: .module)
   static let primary700 = ColorAsset(named: "primary700", bundle: .module)
   static let primary800 = ColorAsset(named: "primary800", bundle: .module)
+  
+  // MARK: Gray
+  static let gray100 = ColorAsset(named: "gray100", bundle: .module)
+  static let gray300 = ColorAsset(named: "gray300", bundle: .module)
+  static let gray500 = ColorAsset(named: "gray500", bundle: .module)
+  static let gray600 = ColorAsset(named: "gray600", bundle: .module)
+  static let gray800 = ColorAsset(named: "gray800", bundle: .module)
 }
 
 #if canImport(SwiftUI)
