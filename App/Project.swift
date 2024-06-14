943c4b8dbe76c86cfc5700f8ad43fcd7984dc376
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-15 00:38:59 +0900

    Feat/config navigation bar and tab bar appearance (#12)
    * [Add] App 모듈에 DesignSystem 모듈 의존성 추가
    
    * [Add] 탭 바 및 내비게이션 바 외형 설정 추가
    
    * 탭 바에 틴트, 배경 색상 설정
    * 내비게이션 바에 텍스트 색상, 배경 색상 설정
    * 예시 코드 추가

--- App/Project.swift
+++ App/Project.swift
@@ -2,5 +2,8 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.makeApp(
-  name: ModuleNameSpace.App.App.rawValue
+  name: ModuleNameSpace.App.App.rawValue,
+  dependencies: [
+    .Project.DesignSystem,
+  ]
 )
