28197ea4a9877ad55fbfe22384870e7cf352ae8d
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-14 16:32:21 +0900

    [Add] CookNow BottomSheet 메서드 확장 구현
    * sheet 내부 contentView 상 24, 좌우 18.5, 하 35 패딩 적용
    * 내부 컨텐츠 사이즈에 따라 sheet 사이즈 정해짐. 내부 컨텐츠 사이즈 관계없이 기본적으로 시트가 디바이스 가로 너비만큼을 가지도록 하기 위해 ZStack으로 컨테이닝후 maxWidth값 최대로 조정

---
+++ UI/DesignSystem/Sources/Foundation+Constants/Components/BottonSheet/CNSheet.swift
@@ -0,0 +1,57 @@
+//
+//  CNSheet.swift
+//  DesignSystem
+//
+//  Created by 쩡화니 on 6/14/24.
+//
+
+import SwiftUI
+import PopupView
+import DesignSystemFoundation
+
+public extension View {
+  func cnSheet<PopupContent: View>(
+    isPresented: Binding<Bool>,
+    @ViewBuilder view: @escaping () -> PopupContent
+  ) -> some View {
+    self
+      .popup(isPresented: isPresented) {
+        ZStack {
+          view()
+            .padding(.top, 24)
+            .padding(.horizontal, 18.5)
+            .padding(.bottom, 35)
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.asset(.white))
+      } customize: {
+        $0
+          .type(.toast)
+          .position(.bottom)
+          .dragToDismiss(true)
+          .backgroundColor(Color.asset(.overlayBackground))
+      }
+  }
+}
+
+#Preview {
+  VStack {
+    Text("바탕")
+  }
+  .cnSheet(isPresented: .constant(true)) {
+    VStack(alignment: .center, spacing: 0) {
+      CNCalendarView()
+        .aspectRatio(338/356, contentMode: .fit)
+        .frame(maxWidth: .infinity)
+      HStack(alignment: .center, spacing: 10) {
+        Button(action: {}) {
+          Text("취소")
+        }.buttonStyle(StateButtonStyle.secondary(.default))
+        Button(action: {}) {
+          Text("수정")
+        }.buttonStyle(StateButtonStyle.primary(.default))
+          .disabled(true)
+      }
+    }
+  }
+}
