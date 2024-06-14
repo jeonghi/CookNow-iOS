a7ba1a4856d282a6ec483472c184b7a9964a0c4d
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-15 00:38:35 +0900

    [Add] 탭 바 및 내비게이션 바 외형 설정 추가
    * 탭 바에 틴트, 배경 색상 설정
    * 내비게이션 바에 텍스트 색상, 배경 색상 설정
    * 예시 코드 추가

---
+++ App/Sources/Application/AppAppearance.swift
@@ -0,0 +1,88 @@
+//
+//  AppAppearance.swift
+//  App
+//
+//  Created by 쩡화니 on 6/14/24.
+//
+
+import UIKit
+import DesignSystem
+import DesignSystemFoundation
+
+enum AppAppearance {
+  static func configure() {
+    configureTabBarAppearance()
+    configureNavigationBarApperance()
+  }
+  
+  private static func configureNavigationBarApperance() {
+    
+    // UINavigationBarApperance
+    let appearance = UINavigationBarAppearance()
+    
+    let titleTextAttributes: [NSAttributedString.Key: Any] = [
+      .foregroundColor: UIColor.asset(.black),
+    ]
+    
+    appearance.titleTextAttributes = titleTextAttributes
+    appearance.backgroundColor = .asset(.white)
+    
+    // UINavigationBar.appearance()
+    let navigationBarAppearance = UINavigationBar.appearance()
+    navigationBarAppearance.standardAppearance = appearance
+    navigationBarAppearance.scrollEdgeAppearance = appearance
+  }
+  
+  private static func configureTabBarAppearance() {
+    
+    // UITabBarAppearance()
+    let appearance = UITabBarAppearance()
+    
+    appearance.configureWithOpaqueBackground()
+    appearance.backgroundColor = .asset(.white)
+    
+    // UITabBar.appearance()
+    let tabBarAppearance = UITabBar.appearance()
+    
+    tabBarAppearance.tintColor = UIColor.asset(.primary700)
+    tabBarAppearance.isTranslucent = false
+    tabBarAppearance.standardAppearance = appearance
+    tabBarAppearance.scrollEdgeAppearance = appearance
+//    tabBarAppearance.layer.cornerRadius = 30
+//    tabBarAppearance.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+  }
+}
+
+#if(DEBUG)
+
+#Preview {
+  AppAppearance.configure()
+  
+  let rootVC = UITabBarController()
+  
+  let vc1 = UIViewController()
+  let vc2 = UIViewController()
+  
+  vc1.view.backgroundColor = .darkGray
+  vc2.view.backgroundColor = .lightGray
+  
+  vc1.tabBarItem = UITabBarItem(
+    title: "Home",
+    image: UIImage(systemName: "house.fill"),
+    tag: 0
+  )
+  vc2.tabBarItem = UITabBarItem(
+    title: "Settings",
+    image: UIImage(
+      systemName: "gearshape.fill"
+    ),
+    tag: 1
+  )
+  
+  rootVC.setViewControllers([vc1, vc2], animated: false)
+  
+  let navVC = UINavigationController(rootViewController: rootVC)
+  return navVC
+}
+
+#endif
