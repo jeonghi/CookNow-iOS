d4bdec1ca8ba8efae2fffc24d430c7d690cedb57
jeonghi <jpark0902@kookmin.ac.kr>
2024-06-14 15:16:20 +0900

    [Add] 추가된 폰트 가이드 반영 (#7)

--- UI/DesignSystem/Sources/Foundation+Constants/Assets/FontAsset+Constants.swift
+++ UI/DesignSystem/Sources/Foundation+Constants/Assets/FontAsset+Constants.swift
@@ -38,11 +38,35 @@ public extension FontAsset.Leading {
 }
 
 public extension FontAsset {
+  
+  // title
+  static var logoTitle: FontAsset = .init(.bold, size: 38)
+  
+  // headline
+  static var headline1: FontAsset = .init(.bold, size: 20)
+  
+  // subheadline
+  static var subhead3: FontAsset = .init(.bold, size: 16)
+  static var subhead2: FontAsset = .init(.bold, size: 14)
+  static var subhead1: FontAsset = .init(.bold, size: 12)
+  
+  // body
+  static var body3: FontAsset = .init(.regular, size: 16)
+  static var bodyBold3: FontAsset = .init(.bold, size: 16)
+  
+  static var body2: FontAsset = .init(.regular, size: 14)
+  static var bodyBold2: FontAsset = .init(.bold, size: 14)
+  
+  static var body1: FontAsset = .init(.regular, size: 12)
+  static var bodyBold1: FontAsset = .init(.bold, size: 12)
+  
+  static var caption: FontAsset = .init(.regular, size: 12)
+  
+  // Legacy
   static var title1: FontAsset = .init(.bold, size: 22)
   static var title2: FontAsset = .init(.bold, size: 14)
   static var bodyBold: FontAsset = .init(.bold, size: 13)
   static var body: FontAsset = .init(.regular, size: 13)
-  static var caption: FontAsset = .init(.regular, size: 12)
 }
 
 #if canImport(SwiftUI)
