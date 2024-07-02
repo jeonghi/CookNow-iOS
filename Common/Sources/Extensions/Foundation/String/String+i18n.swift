1b19ce6343de4cf63aef8d01b805a8cd970f1f24
jeonghi <jpark0902@kookmin.ac.kr>
2024-07-02 13:57:38 +0900

    [Add] localization 위한 String Extension 추가

---
+++ Common/Sources/Extensions/Foundation/String/String+i18n.swift
@@ -0,0 +1,22 @@
+//
+//  String+i18n.swift
+//  Common
+//
+//  Created by 쩡화니 on 7/2/24.
+//
+
+import Foundation
+
+public extension String {
+  var localized: String {
+    return NSLocalizedString(self, comment: "")
+  }
+  
+  func localized(_ with: String) -> String {
+    return String(format: self.localized, with)
+  }
+  
+  func localized(_ number: Int) -> String {
+    return String(format: self.localized, number)
+  }
+}
