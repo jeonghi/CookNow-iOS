51330379f9d1117433fa62e55b51b76c1ff984d4
jeonghi <jpark0902@kookmin.ac.kr>
2024-07-21 17:15:25 +0900

    [Feat] 앱 모듈에 파이어베이스 및 구글 로그인 관련 패키지 의존성 추가

--- App/Project.swift
+++ App/Project.swift
@@ -5,5 +5,8 @@ let project = Project.makeApp(
   name: ModuleNameSpace.App.App.rawValue,
   dependencies: [
     .Project.DesignSystem,
+    .ExternalProject.Google.FirebaseAnalytics,
+    .ExternalProject.Google.FirebaseAuth,
+    .ExternalProject.Google.GoogleSignIn
   ]
 )
