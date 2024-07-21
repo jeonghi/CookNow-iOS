7e2d71cdbbc4ab8169aa9f1f154362d35119014a
jeonghi <jpark0902@kookmin.ac.kr>
2024-07-21 17:14:25 +0900

    [Modify] 앱 번들 수정

--- Tuist/ProjectDescriptionHelpers/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Environment.swift
@@ -8,8 +8,8 @@
 import ProjectDescription
 
 public extension Environment {
-  static let organizationName = "com.jeonghi"
-  static let appName = "CookNow"
+  static let organizationName = "com.cooknow"
+  static let appName = "app"
   static let bundleId = "\(organizationName).\(appName)"
   static let forPreview = true
 }
