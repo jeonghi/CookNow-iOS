db38625a902029b7a3cb1523905a816578b223a5
jeonghi <jpark0902@kookmin.ac.kr>
2024-07-24 01:11:35 +0900

    [Feat] Add AuthAPI for sign-in, sign-out, and token refresh

---
+++ Core/CNNetwork/Sources/AuthAPI.swift
@@ -0,0 +1,84 @@
+//
+//  AuthAPI.swift
+//  CNNetwork
+//
+//  Created by 쩡화니 on 7/23/24.
+//
+
+import Foundation
+import Alamofire
+
+public enum AuthAPI {
+  /// 로그인
+  case signIn(SignInDTO.Request)
+  /// 로그아웃
+  case signOut
+  /// 토큰 갱신하기
+  case refreshToken
+}
+
+extension AuthAPI: TargetType {
+  
+  public var baseURL: String {
+    Constants.authBaseUrl
+  }
+  
+  public var method: Alamofire.HTTPMethod {
+    switch self {
+    case .signIn:
+      return .post
+    case .signOut:
+      return .post
+    case .refreshToken:
+      return .post
+    }
+  }
+  
+  public var path: String {
+    switch self {
+    case .signIn:
+      return "/oauth/sign-in"
+    case .signOut:
+      return ""
+    case .refreshToken:
+      return ""
+    }
+  }
+  
+  public var header: [String : String] {
+    switch self {
+    case .signIn(let request):
+      return [
+        HTTPHeader.authorization.rawValue: "Bearer \(request.idToken)",
+        HTTPHeader.contentType.rawValue: HTTPHeader.json.rawValue
+      ]
+    default:
+      return [:]
+    }
+  }
+  
+  public var parameters: String? {
+    nil
+  }
+  
+  public var queryItems: Encodable? {
+    nil
+  }
+  
+  public var body: Data? {
+    let encoder = JSONEncoder()
+    switch self {
+    default:
+      return nil
+    }
+  }
+  
+  public var sessionType: SessionType {
+    switch self {
+    case .signIn, .signOut:
+      return .Auth
+    case .refreshToken:
+      return .AuthApi
+    }
+  }
+}
---
+++ Core/CNNetwork/Sources/DTO/SigninDTO.swift
@@ -0,0 +1,23 @@
+//
+//  SigninDTO.swift
+//  CNNetwork
+//
+//  Created by 쩡화니 on 7/23/24.
+//
+
+import Foundation
+
+public enum SignInDTO {
+  public struct Request: Encodable {
+    let idToken: String
+    
+    public init(_ idToken: String) {
+      self.idToken = idToken
+    }
+  }
+  
+  public struct Response: Decodable {
+    let accessToken: String
+    let refreshToken: String
+  }
+}
