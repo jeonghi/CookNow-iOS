2d3537d944aac515be6aa8d89e0c016a9af63460
jeonghi <jpark0902@kookmin.ac.kr>
2024-08-14 09:21:25 +0900

    [Feat] View bound 얻을 수 있도록 하는 View 메서드 익스텐션 정의

---
+++ Common/Sources/Extensions/SwiftUI/View/View+getRect.swift
@@ -0,0 +1,14 @@
+//
+//  View+getRect.swift
+//  Common
+//
+//  Created by 쩡화니 on 8/12/24.
+//
+
+import SwiftUI
+
+public extension View {
+  func getRect() -> CGRect {
+    return UIScreen.main.bounds
+  }
+}
