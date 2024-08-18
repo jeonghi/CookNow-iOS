1ac5afd289c8901e44653d82290bf58931fae493
jeonghi <jpark0902@kookmin.ac.kr>
2024-08-18 18:53:43 +0900

    [Feat] 인증 라우터에 signout(로그아웃) 엔드포인트 추가 및 로그아웃 DTO 정의

---
+++ Core/CNNetwork/Sources/DTO/SignOutDTO.swift
@@ -0,0 +1,14 @@
+//
+//  SignOutDTO.swift
+//  CNNetwork
+//
+//  Created by 쩡화니 on 8/18/24.
+//
+
+import Foundation
+
+public enum SignOutDTO {
+  public struct Response: Decodable {
+    let result: String?
+  }
+}
