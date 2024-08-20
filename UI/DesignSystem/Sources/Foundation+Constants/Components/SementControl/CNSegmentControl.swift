d1924142c367b10d62dbe4bccf7bbc075886aa86
jeonghi <jpark0902@kookmin.ac.kr>
2024-08-20 20:52:49 +0900

    [Feat] 재료함 네트워크 모듈 연동

--- UI/DesignSystem/Sources/Foundation+Constants/Components/SementControl/CNSegmentControl.swift
+++ UI/DesignSystem/Sources/Foundation+Constants/Components/SementControl/CNSegmentControl.swift
@@ -9,45 +9,51 @@ import SwiftUI
 import DesignSystemFoundation
 import Common
 
-
-public struct CNSegmentControl<T>: View where T: RawRepresentable, T.RawValue == String, T: Equatable, T: Hashable {
+public struct CNSegmentControl<T>: View where T: Identifiable, T: Hashable {
   
   let segments: [T]
   @Binding private var selected: T
   @Namespace private var namespace
+  private let label: (T) -> String
   
   public init(
     segments: [T],
-    selected: Binding<T>
+    selected: Binding<T>,
+    label: @escaping (T) -> String
   ) {
     self.segments = segments
     self._selected = selected
+    self.label = label
   }
   
   public var body: some View {
-    HStack(spacing: 0) {
-      ForEach(self.segments, id: \.self) { segment in
-        Button(action: {
-          self.selected = segment
-        }) {
-          VStack {
-            Text(segment.rawValue)
-              .padding(.vertical, 5.5)
-              .padding(.horizontal, 28)
-            ZStack(alignment: .bottom) {
-              Capsule()
-                .fill(Color.clear)
-                .frame(height: 0.1)
-              if selected == segment {
-                Rectangle()
-                  .frame(height: 2)
-                  .matchedGeometryEffect(id: "tab", in: namespace)
-              }
+    ScrollView(.horizontal, showsIndicators: false) {
+      LazyHStack(spacing: 0) {
+        ForEach(self.segments, id: \.self) { segment in
+          Button(action: {
+            self.selected = segment
+          }) {
+            VStack {
+              Text(label(segment))
+                .padding(.vertical, 5.5)
+                .padding(.horizontal, 28)
             }
-            .background(Color.asset(.clear))
-          }
-          .applyIf(segment == self.selected) {
-            $0.tint(Color.asset(.primary700))
+            .overlay(alignment: .bottom) {
+              ZStack(alignment: .bottom) {
+                Capsule()
+                  .fill(Color.clear)
+                  .frame(height: 0.1)
+                if selected == segment {
+                  Rectangle()
+                    .frame(height: 2)
+                    .matchedGeometryEffect(id: "tab", in: namespace)
+                }
+              }
+              .background(Color.asset(.clear))
+            }
+            .applyIf(segment == self.selected) {
+              $0.tint(Color.asset(.primary700))
+            }
           }
         }
       }
@@ -56,20 +62,3 @@ public struct CNSegmentControl<T>: View where T: RawRepresentable, T.RawValue ==
     .tint(Color.asset(.gray500))
   }
 }
-
-#if(DEBUG)
-enum Foods: String, CaseIterable {
-  case 과일
-  case 채소
-  case 고기
-}
-
-
-#Preview {
-  @State var selected = Foods.고기
-  return CNSegmentControl(
-    segments: Foods.allCases,
-    selected: $selected
-  )
-}
-#endif
