6900f61d49bf75ffc74453726a3b73ffe8616fbb
jeonghi <jpark0902@kookmin.ac.kr>
2024-08-20 20:49:30 +0900

    [Fix] Auth 의존성 문제로 Auth 관련 종속성 제거

--- Core/CNNetwork/Sources/Common/Interceptors/AuthApiRequestAdapter.swift
+++ Core/CNNetwork/Sources/Common/Interceptors/AuthApiRequestAdapter.swift
@@ -7,11 +7,8 @@
 
 import Foundation
 import Alamofire
-import Auth
 
 final class AuthApiRequestAdapter: RequestInterceptor {
-  
-  let tokenManager: TokenManagerType = TokenManager.shared
   
   init() {
   }
@@ -21,10 +18,6 @@ final class AuthApiRequestAdapter: RequestInterceptor {
     
     // TODO: 기본 Api 요청할 때 필요한 기능 추가 (예: API서버 인증 키 헤더에 추가 등..)
     urlRequest.addValue(HTTPHeader.json.rawValue, forHTTPHeaderField: HTTPHeader.contentType.rawValue)
-    
-    guard let token = tokenManager.getToken() else {
-      return
-    }
             
     completion(.success(urlRequest))
   }
