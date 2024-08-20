b675a9b69ff251fee87183db6fb78b5d96f61967
jeonghi <jpark0902@kookmin.ac.kr>
2024-08-20 20:22:18 +0900

    [Move] 디렉토리 구조 변경

---
+++ Core/CNNetwork/Sources/Common/Interceptors/ApiRequestAdapter.swift
@@ -0,0 +1,22 @@
+//
+//  ApiRequestAdapter.swift
+//  CNNetwork
+//
+//  Created by 쩡화니 on 7/23/24.
+//
+
+import Foundation
+import Alamofire
+
+final class ApiRequestAdapter: RequestInterceptor {
+  
+  init() {
+  }
+  
+  func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, any Error>) -> Void) {
+    var urlRequest = urlRequest
+    
+    // TODO: 기본 Api 요청할 때 필요한 기능 추가 (예: API서버 인증 키 헤더에 추가 등..)
+    completion(.success(urlRequest))
+  }
+}
--- Core/CNNetwork/Sources/Common/Interceptors/AuthApiRequestAdapter.swift
+++ Core/CNNetwork/Sources/Common/Interceptors/AuthApiRequestAdapter.swift
@@ -1,14 +1,17 @@
 //
-//  ApiRequestAdapter.swift
+//  AuthApiRequestAdapter.swift
 //  CNNetwork
 //
-//  Created by 쩡화니 on 7/23/24.
+//  Created by 쩡화니 on 8/20/24.
 //
 
 import Foundation
 import Alamofire
+import Auth
 
-final class ApiRequestAdapter: RequestInterceptor {
+final class AuthApiRequestAdapter: RequestInterceptor {
+  
+  let tokenManager: TokenManagerType = TokenManager.shared
   
   init() {
   }
@@ -17,7 +20,12 @@ final class ApiRequestAdapter: RequestInterceptor {
     var urlRequest = urlRequest
     
     // TODO: 기본 Api 요청할 때 필요한 기능 추가 (예: API서버 인증 키 헤더에 추가 등..)
+    urlRequest.addValue(HTTPHeader.json.rawValue, forHTTPHeaderField: HTTPHeader.contentType.rawValue)
     
+    guard let token = tokenManager.getToken() else {
+      return
+    }
+            
     completion(.success(urlRequest))
   }
 }
