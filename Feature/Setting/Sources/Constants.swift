a58d163a686da11c03aee6f396608e26b0b71ce4
jeonghi <jpark0902@kookmin.ac.kr>
2024-09-12 22:49:54 +0900

    [Chore] 고객센터, 약관동의 웹 페이지 url 설정

---
+++ Feature/Setting/Sources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Setting
+//
+//  Created by 쩡화니 on 9/12/24.
+//
+
+import Foundation
+import Common
+
+final class Constants {
+  static var csWebUrl = "https://abrasive-sushi-3a1.notion.site/bdeaf49e716f4a4a854298d239bda86e?pvs=4"
+  static var tosWebUrl = "https://abrasive-sushi-3a1.notion.site/Cooknow-e630913fb4214c238786b5ed0c7e655e?pvs=4"
+}
