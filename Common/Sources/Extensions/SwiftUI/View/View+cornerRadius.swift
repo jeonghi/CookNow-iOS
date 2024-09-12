04d38694a065eb9f73775981813adae464424146
jeonghi <jpark0902@kookmin.ac.kr>
2024-09-12 22:45:55 +0900

    [Feat] View 모서리에 radius 주는 메서드 추가

---
+++ Common/Sources/Extensions/SwiftUI/View/View+cornerRadius.swift
@@ -0,0 +1,29 @@
+//
+//  View+cornerRadius.swift
+//  Common
+//
+//  Created by 쩡화니 on 9/12/24.
+//
+
+import SwiftUI
+
+public extension View {
+  /// 특정 모서리 cornerRadius
+  func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+    clipShape(RoundedCorner(radius: radius, corners: corners))
+  }
+}
+
+private struct RoundedCorner: Shape {
+  var radius: CGFloat = .infinity
+  var corners: UIRectCorner = .allCorners
+  
+  func path(in rect: CGRect) -> Path {
+    let path = UIBezierPath(
+      roundedRect: rect,
+      byRoundingCorners: corners,
+      cornerRadii: CGSize(width: radius, height: radius)
+    )
+    return Path(path.cgPath)
+  }
+}
