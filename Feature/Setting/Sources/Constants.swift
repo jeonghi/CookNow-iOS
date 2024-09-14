076543493363c932d67c7f5aaa847e266cf1353d
jeonghi <jpark0902@kookmin.ac.kr>
2024-09-14 18:50:23 +0900

    [Modify] url 상수 데이터 타입 URL? 로 수정

--- Feature/Setting/Sources/Constants.swift
+++ Feature/Setting/Sources/Constants.swift
@@ -9,6 +9,6 @@ import Foundation
 import Common
 
 final class Constants {
-  static var csWebUrl = "https://abrasive-sushi-3a1.notion.site/bdeaf49e716f4a4a854298d239bda86e?pvs=4"
-  static var tosWebUrl = "https://abrasive-sushi-3a1.notion.site/Cooknow-e630913fb4214c238786b5ed0c7e655e?pvs=4"
+  static var csWebUrl = "https://abrasive-sushi-3a1.notion.site/bdeaf49e716f4a4a854298d239bda86e?pvs=4".asUrl()
+  static var tosWebUrl = "https://abrasive-sushi-3a1.notion.site/Cooknow-e630913fb4214c238786b5ed0c7e655e?pvs=4".asUrl()
 }
