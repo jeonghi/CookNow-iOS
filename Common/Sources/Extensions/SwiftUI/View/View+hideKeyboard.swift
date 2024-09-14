87ab83a87ce0cc5b884bb17ed4ef728b6fc7fbe1
jeonghi <jpark0902@kookmin.ac.kr>
2024-09-14 20:25:57 +0900

    [Refactor] 불필요한 코드 제거

--- Common/Sources/Extensions/SwiftUI/View/View+hideKeyboard.swift
+++ Common/Sources/Extensions/SwiftUI/View/View+hideKeyboard.swift
@@ -12,12 +12,7 @@ public extension View {
   /// View 주변 터치시 키보드 내리기
   func hideKeyboardWhenTappedAround() -> some View {
     return self.onTapGesture {
-      UIApplication.shared.sendAction(
-        #selector(UIResponder.resignFirstResponder),
-        to: nil,
-        from: nil,
-        for: nil
-      )
+      hideKeyboard()
     }
   }
   
